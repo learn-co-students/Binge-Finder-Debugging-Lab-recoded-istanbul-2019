ff5a6af7215abf968176b3c4e5a65f79dcb450ce
naderalfakesh <nader.alfakesh@gmail.com>
2020-03-05 07:44:09 +0300

    Done.

--- src/Components/TVShow.js
+++ src/Components/TVShow.js
@@ -4,7 +4,7 @@ const tvShow = (props) => {
   return (
     <div>
       <br/>
-      <img src={props.image.medium} onClick={props.selectShow} alt=""/>
+      <img src={props.show.image.medium} onClick={()=>props.selectShow(props.show)} alt=""/>
     </div>
   );
 }
--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -3,16 +3,16 @@ import {Grid} from 'semantic-ui-react';
 import TVShow from './TVShow';
 
 class TVShowList extends Component {
-
-  mapAllShows = (props) => {
-    if (!!props.searchTerm){
-      props.shows.map((s) => {
-        if (s.name.toLowerCase().includes(props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> )
+ 
+  mapAllShows = () => {
+    if (!!this.props.searchTerm){
+      this.props.shows.map((s) => {
+        if (s.name.toLowerCase().includes(this.props.searchTerm)){
+          (<TVShow show={s} key={s.id} selectShow={this.props.selectShow}/> )
         }
       })
     }
-    return props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={props.selectShow}/>)
+    return this.props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={this.props.selectShow}/>)
   }
 
   render() {

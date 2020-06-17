464d7d019bfd9489469e6f588d343dbdf6024efd
kamalbakour <kamal.bak92@gmail.com>
2020-06-17 21:31:56 +0300

    Done.

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { Input } from 'semantic-ui-react';
 
 const Search = (props) => {
+  //console.log(props)
   return (
     <div>
-      <Input type="text" placeholder="Search" onChange={props.handleSearch} value={props.search} />
+      <Input type="text" placeholder="Search" onChange={(e) => props.handlesearch(e)} value={props.search} />
     </div>
   )
 }
--- src/Components/TVShowList.js
+++ src/Components/TVShowList.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import {Grid} from 'semantic-ui-react';
+import React, { useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import TVShow from './TVShow'
 
-class TVShowList extends Component {
-
-  mapAllShows = () => {
-    if (!!props.searchTerm){
-      props.shows.map((s) => {
-        if (s.name.toLowerCase().includes(props.searchTerm)){
-          (<TVShow show={s} key={s.id} selectShow={props.selectShow}/> )
-        }
+const TVShowList = (props) => {
+  //console.log(props)
+  const mapAllShows = () => {
+    if (!!props.searchTerm) {
+      return props.shows.map((s) => {
+        if (s.name.toLowerCase().includes(props.searchTerm)) {
+          //console.log(`${s.name} is matched`)
+          return <TVShow show={s} key={s.id} selectShow={props.selectShow} />;
+        } /*else {
+          return console.log('nothing matched');
+        }*/
       })
+    } else {
+      return props.shows.map((s) => <TVShow show={s} key={s.id} selectShow={props.selectShow} />)
     }
-    return props.shows.map( (s)=> <TVShow show={s} key={s.id} selectShow={props.selectShow}/>)
   }
 
-  render() {
-    return (
-      <div className="TVShowList">
-        <Grid>
-          {this.mapAllShows()}
-        </Grid>
-      </div>
-    )
-  }
+  return (
+    <div className="TVShowList">
+      <Grid>
+        {mapAllShows()}
+      </Grid>
+    </div>
+  )
+
 
 }
 
